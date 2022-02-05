ccef62102cd1bc9cadccc7c16d82d87b20c6bbce
이자훈 <jh.lee@break.co.kr>
2022-02-05 20:03:12 +0900

    notification

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Picture from './Picture';
+import Noticiation from './Noticiation';
 
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => Picture);
+AppRegistry.registerComponent(appName, () => Noticiation);
